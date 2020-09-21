16a9cf72c4dcbc244237b55d50b67802f9c25008
adrienmeli <adrienmeli@gmail.com>
2020-09-21 15:58:59 +0200

    New commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ev
+
+Rscript -e "rmarkdown::render_site()"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "source('moodlize.R')"
+
+# let's zip the file for upload
+cp -a _site/. /home/adrien/github/adrienmeli.github.io/
+cd /home/adrien/github/adrienmeli.github.io/
+git checkout gh-pages
+git add .
+git commit -m "New commit"
+git push -u origin gh-pages
+
